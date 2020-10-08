f6a67cd3384a7c4a82d5ffd7848e98df99da7403
HAYOUNG LEE <l4538@naver.com>
2020-10-08 23:28:35 +0900

    complete to make kafka prod

--- src/crawling/kafka_producer.py
+++ src/crawling/kafka_producer.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[ ]:
 
 
 import random
 import string
 from time import sleep
 from kafka import KafkaProducer
+import json
 
 string_pool = string.ascii_lowercase + string.digits
 company_pool = ["현대자동차","삼성전자","LG전자","네이버","카카오","SK텔레콤"]
 
-addr = ['kafka:9092']
+
+with open('./config/db.json') as json_file:
+    config_data = json.load(json_file)
+
+
+addr = [config_data['addr']+':9092']
+
 topicName = 'test'
+
 producer = KafkaProducer(bootstrap_servers=addr,
-                         value_serializer=lambda x: 
-                         dumps(x).encode('utf-8'))
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+                        api_version=(0,10))
 
 
 def make_ID():
@@ -49,13 +57,14 @@ def make_log(n):
     return log_list
 
 
-for i in range(50):
+for i in range(5):
     sleep_time = random.randint(1,5)
     cnt = random.randint(9,99)
     log_list = make_log(cnt)
     
     for log in log_list:
-        print(i, log)
+        #print(i, log)
+        producer.send(topicName, value = log)
     
     sleep(sleep_time)
 
